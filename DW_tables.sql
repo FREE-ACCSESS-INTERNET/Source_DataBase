7c0710a9c46035fad1c178dab3c9a76c225b7301
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2024-06-27 21:45:59 +0330

    add table data WareHouse

---
+++ DW_tables.sql
@@ -0,0 +1,179 @@
+-- create the table
+
+CREATE TABLE [dbo].[Status] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE [dbo].[Wallets] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Balance] DECIMAL(18, 2) NOT NULL,
+);
+
+CREATE TABLE [dbo].[Cards] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [WalletId] INT NOT NULL,
+    [CardNumber] NVARCHAR(50) NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    FOREIGN KEY (WalletId) REFERENCES Wallets(Id)
+);
+
+CREATE TABLE [dbo].[Users] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL,
+    [TelgramId] NVARCHAR(50) NOT NULL,
+    [TelegramInfo] NVARCHAR(200) NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [WalletId] INT NOT NULL,
+    [InviteCode] NVARCHAR(50) NOT NULL,
+    FOREIGN KEY (WalletId) REFERENCES Wallets(Id)
+);
+
+CREATE TABLE [dbo].[ReferralStatus] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Status] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [ReferralId] INT NOT NULL,
+    FOREIGN KEY (ReferralId) REFERENCES Users(Id),
+    FOREIGN KEY (Status) REFERENCES Status(Id)
+);
+
+
+CREATE TABLE [dbo].[Referrals] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [UserId] INT NOT NULL,
+    [ReferralId] INT NOT NULL,
+    [CardID] INT NOT NULL,
+    [Balance] DECIMAL(18, 2) NOT NULL,
+    FOREIGN KEY (CardID) REFERENCES Cards(Id),
+    FOREIGN KEY (UserId) REFERENCES Users(Id),
+    FOREIGN KEY (ReferralId) REFERENCES Users(Id),
+);
+
+CREATE TABLE [dbo].[Countries] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE [dbo].[Server] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL,
+    [IP] NVARCHAR(50) NOT NULL,
+    [Port] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [Status] INT NOT NULL,
+    [CountryId] INT NOT NULL,
+    FOREIGN KEY (CountryId) REFERENCES Countries(Id),
+    FOREIGN KEY (Status) REFERENCES Status(Id)
+);
+
+CREATE TABLE [dbo].[Payments] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [BuyerWalletId] INT NOT NULL,
+    [SellerCardId] INT NOT NULL,
+    [Amount] DECIMAL(18, 2) NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    FOREIGN KEY (BuyerWalletId) REFERENCES Wallets(Id),
+    FOREIGN KEY (SellerCardId) REFERENCES Cards(Id)
+);
+
+CREATE TABLE [dbo].[PaymentsStatus] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Status] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [PaymentId] INT NOT NULL,
+    FOREIGN KEY (PaymentId) REFERENCES Payments(Id),
+    FOREIGN KEY (Status) REFERENCES Status(Id)
+);
+
+CREATE TABLE [dbo].[Paths] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ServerId] INT NOT NULL,
+    [Address] NVARCHAR(50) NOT NULL,
+    [Info] NVARCHAR(200) NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [Status] INT NOT NULL,
+    [PricePerGig] DECIMAL(18, 2) NOT NULL,
+    FOREIGN KEY (ServerId) REFERENCES Server(Id),
+    FOREIGN KEY (Status) REFERENCES Status(Id)
+);
+
+CREATE TABLE [dbo].[Configurations] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [WalletId] INT NOT NULL,
+    [ServerId] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [Status] INT NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL,
+    [ConfTemplate] NVARCHAR(200) NOT NULL,
+    [UsedGig] INT NOT NULL,
+    [MaxGig] INT NOT NULL,
+    [ActivePathId] INT NOT NULL,
+    FOREIGN KEY (ActivePathId) REFERENCES Paths(Id),
+    FOREIGN KEY (WalletId) REFERENCES Wallets(Id),
+    FOREIGN KEY (ServerId) REFERENCES Server(Id),
+    FOREIGN KEY (Status) REFERENCES Status(Id)
+);
+
+CREATE TABLE [dbo].[LastUsedGig] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ConfigurationId] INT NOT NULL,
+    [UsedGig] DECIMAL(3, 2) NOT NULL,
+    [UpdateDate] DATETIME,
+    FOREIGN KEY (ConfigurationId) REFERENCES Configurations(Id)
+);
+
+
+CREATE TABLE [dbo].[PathStatus] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [PathId] INT NOT NULL,
+    [Speed] INT NOT NULL,
+    [Ping] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    FOREIGN KEY (PathId) REFERENCES Paths(Id)
+);
+
+CREATE TABLE [dbo].[Traffics] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ConfigurationId] INT NOT NULL,
+    [PathId] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [Gig] DECIMAL(3, 2) NOT NULL,
+    FOREIGN KEY (ConfigurationId) REFERENCES Configurations(Id),
+    FOREIGN KEY (PathId) REFERENCES Paths(Id)
+);
+
+CREATE TABLE [dbo].[Transactions] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [TrafficsId] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    FOREIGN KEY (TrafficsId) REFERENCES Traffics(Id)
+);
+
+CREATE TABLE [dbo].[SubTransactions] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [TransactionId] INT NOT NULL,
+    [Amount] DECIMAL(18, 2) NOT NULL,
+    [ReferralId] INT NOT NULL,
+    [SellerWalletId] INT NOT NULL,
+    [BuyerWalletId] INT NOT NULL,
+    [Status] INT NOT NULL,
+    FOREIGN KEY (Status) REFERENCES Status(Id),
+    FOREIGN KEY (TransactionId) REFERENCES Transactions(Id),
+    FOREIGN KEY (ReferralId) REFERENCES Referrals(Id),
+    FOREIGN KEY (SellerWalletId) REFERENCES Wallets(Id),
+    FOREIGN KEY (BuyerWalletId) REFERENCES Wallets(Id)
+);
+
+
+-- create tables for Data WareHouse 
+
+CREATE TABLE [Net].[DimServer] (
+    [Id] INT NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    [IP] NVARCHAR(50) NOT NULL,
+    [Port] INT NOT NULL,
+    [CreatedAt] DATETIME NOT NULL,
+    [Status] NVARCHAR(50) NOT NULL,
+    [CountryName] NVARCHAR(50) NOT NULL
+);
